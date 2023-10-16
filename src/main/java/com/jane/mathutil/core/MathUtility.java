c1554585a5c70ff46979b6aa07ee7e0ece07778f
J1410T <janetran2003@gmail.com>
2023-10-16 14:56:31 +0700

    update src to use recursion and data driven testing and green4ever again

--- src/main/java/com/jane/mathutil/core/MathUtility.java
+++ src/main/java/com/jane/mathutil/core/MathUtility.java
@@ -19,16 +19,64 @@ public class MathUtility {
     public static long getFactorial(int n){
         if(n<0||n>20)
             throw new IllegalArgumentException("Invalid n , n must " + " be between 0..20");
+        
         if(n==0||n==1)
-            return 1; //nếu có thể kết thúc sớm, thì kết thúc ngay
-        //sống sots đến chỗ này, sure n =2..20 vì nếu k chết dọc đường ở 2 cụm if ở trên rồi
-        //k cần else nếu trước đó mà return 
-        long result = 10; //giải thừa khởi đầu là 1
-        //nhân đôi 2 3 4 5 ...n vào biến này thì thành n!
-        for(int i = 2; i <= n; i++){
-            result *= i;
-        }
-        return result;
+            return 1; 
+        
+        return n * getFactorial(n-1); //đệ quy xong
     }
+    
+    
+    //ĐỆ QUY - RECURSION
+    //BÚP BÊ NGA - ĐỆ QUY LÀ GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN
+    //                        NHỎ ĐẾN 1 MỨC THÌ DỪNG LẠI
+    // 5! = 1.2.3.4.5
+    // 5!       4!x 5 = 5x4!
+    // 4! = 1.2.3.4.5
+    // 4!       3!x 5 = 4x3! đuổi nhau rồi
+    // 3!             = 3x2!
+    // 2!             = 2x1! DỪNG NGAY
+    // N!             = N x (N-1)!
+    
+    
+    
+    // KỸ THUẬT KIỂM THỬ HỒI QUY - REGRESSION TEST
+    // TEST LẠI NHỮNG THỨ ĐÃ TỪNG TEST ĐỂ XÁC NHẬN NÓ CÒN CÓ ỔN KO
+    // TẠI SAO PHẢI TEST LẠI ???
+    // CODE THEO THỜI GIAN SẼ CÓ THAY ĐỔI, THAY ĐỔI VÌ
+    // 1. ANH EM FIX BUG, SẼ SỬA CODE
+    // 2. ANH EM TỐI ƯU, CHỈNH SỬA CODE ĐỂ CODE ĐẸP HƠN, CHẠY TỐT HƠN NHANH HƠN
+    // 3. ANH EM THÊM HÀM MỚI, METHOD MỚI
+    
+    // KHI SỬA CODE, PHẢI TEST LẠI ĐỂ ĐẢM BẢO NÓ VẪN NGON, VẪN XANH
+    // NẾU TEST = TAY CỰC KÌ MẤT SỨC, COI CHỪNG SAI SÓT
+    // NẾU CÓ TEST SCRIPT, TA CHỈ VIỆC RUN, VÀ NHÌN MÀU ĐƯA RA
+    // NẾU MÀU XANH, CODE CHỈNH SỬA ỔN, VÌ THỎA EXPECTED == ACTUAL
+    // NẾU MÀU ĐỎ, CODE KO ỔN, SỬA TIẾP CHO ĐẾN KHI NÀO ỔN
+    // TA TEST LẠI MÀ CHỈ CẦN NHÌN MÀU -> NHANH, KO HAO SỨC, CHÍNH XÁC
+    // MIỄN ĐỦ TEST CASE, TEST SCRIPT
+    
+    
+   // TEST LẠI NHỮNG THỨ ĐÃ TỪNG TEST GỌI LÀ TEST HỒI QUY REGRESSION
+    
+    
+    
+    
+    
+    
+//    public static long getFactorial(int n){
+//        if(n<0||n>20)
+//            throw new IllegalArgumentException("Invalid n , n must " + " be between 0..20");
+//        if(n==0||n==1)
+//            return 1; //nếu có thể kết thúc sớm, thì kết thúc ngay
+//        //sống sots đến chỗ này, sure n =2..20 vì nếu k chết dọc đường ở 2 cụm if ở trên rồi
+//        //k cần else nếu trước đó mà return 
+//        long result = 1; //giải thừa khởi đầu là 1
+//        //nhân đôi 2 3 4 5 ...n vào biến này thì thành n!
+//        for(int i = 2; i <= n; i++){
+//            result *= i;
+//        }
+//        return result;
+//    }
 
 }
